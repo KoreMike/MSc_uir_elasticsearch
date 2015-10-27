faa7830bf922ec6372afc927073c6ccae61427f5
Mike Hewitt <mikerjhewitt@gmail.com>
2015-10-27 11:21:10 +0000

    first setup of institute index

---
+++ tidying_json_files/load_institutes_to_es.js
@@ -0,0 +1,64 @@
+var fs = require('fs');
+var es = require('elasticsearch');
+var client = new es.Client({
+  host: 'localhost:9200'
+});
+
+fs.readFile('uu-institute-index-newline.json', {encoding: 'utf-8'}, function(err, data) {
+  if (err) { throw err; }
+
+  // Build up a giant bulk request for elasticsearch.
+  bulk_request = data.split('\n').reduce(function(bulk_request, line) {
+    var obj, institute;
+
+    try {
+      obj = JSON.parse(line);
+    } catch(e) {
+      console.log('Done reading');
+      return bulk_request;
+    }
+
+    // Rework the data slightly
+    institute = {
+      name: obj.name,
+      source: obj.source,
+      title:obj.title,
+      description:obj.description,
+      keywords:obj.keywords,
+      img:'http://i.imgur.com/PasrpyM.png',
+      url: obj.url,
+    };
+
+    bulk_request.push({index: {_index: 'uu_inst_index', _type: 'institute', _id: institute.url}});
+    bulk_request.push(institute);
+    return bulk_request;
+  }, []);
+
+  // A little voodoo to simulate synchronous insert
+  var busy = false;
+  var callback = function(err, resp) {
+    if (err) { console.log(err); }
+
+    busy = false;
+  };
+
+  // Recursively whittle away at bulk_request, 1000 at a time.
+  var perhaps_insert = function(){
+    if (!busy) {
+      busy = true;
+      client.bulk({
+        body: bulk_request.slice(0, 1000)
+      }, callback);
+      bulk_request = bulk_request.slice(1000);
+      console.log(bulk_request.length);
+    }
+
+    if (bulk_request.length > 0) {
+      setTimeout(perhaps_insert, 10);
+    } else {
+      console.log('Inserted all records.');
+    }
+  };
+
+  perhaps_insert();
+});
